aeab52865ed03c7d0ad4773872c4cb4804047562
Trần Thúy Nga <ngat35137@gmail.com>
2021-11-05 00:16:01 +0700

    up

--- app/src/main/java/com/example/nhom14_appbansachtrennentangandroid/View/MainActivity.java
+++ app/src/main/java/com/example/nhom14_appbansachtrennentangandroid/View/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.nhom14_appbansachtrennentangandroid.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 import com.example.nhom14_appbansachtrennentangandroid.R;
 import com.example.nhom14_appbansachtrennentangandroid.adapter.ViewPagerAdapTer;
 import com.example.nhom14_appbansachtrennentangandroid.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     ViewPagerAdapTer adapter;
     ActivityMainBinding binding;
@@ -23,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbarMain);
         getSupportActionBar().setTitle("");
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbarMain, 0,0);
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        binding.navigationView.setNavigationItemSelectedListener(this);
+
 
         binding.tablayoutMain.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
             @Override
@@ -63,5 +73,19 @@ public class MainActivity extends AppCompatActivity {
         binding.tablayoutMain.addItem(item2);
         binding.tablayoutMain.addItem(item3);
         binding.tablayoutMain.addItem(item4);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        super.onBackPressed();
     }
 }
