19de4920c41504244f0c437c1125790296b01b86
Trần Thúy Nga <ngat35137@gmail.com>
2021-11-20 20:02:14 +0700

    up

--- app/src/main/java/com/example/nhom14_appbansachtrennentangandroid/View/ChiTietSPActivity.java
+++ app/src/main/java/com/example/nhom14_appbansachtrennentangandroid/View/ChiTietSPActivity.java
@@ -10,12 +10,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.bumptech.glide.Glide;
 import com.example.nhom14_appbansachtrennentangandroid.R;
 import com.example.nhom14_appbansachtrennentangandroid.databinding.ActivityChiTietSpactivityBinding;
+import com.example.nhom14_appbansachtrennentangandroid.model.SanPham;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChiTietSPActivity extends AppCompatActivity {
 
@@ -31,6 +36,7 @@ public class ChiTietSPActivity extends AppCompatActivity {
 
         Intent intent=getIntent();
         maSP=intent.getStringExtra("maSP");
+        maSP="sp001";
 
         setSupportActionBar(binding.toolbarSp);
         getSupportActionBar().setTitle("Chi tiết sản phẩm");
@@ -69,6 +75,15 @@ public class ChiTietSPActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        binding.imgQuaylai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        display();
     }
 
     @Override
@@ -87,6 +102,21 @@ public class ChiTietSPActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     private void display(){
+        reference.child("sanpham").child(maSP).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                SanPham sanPham=snapshot.getValue(SanPham.class);
+                binding.tvGiaBan.setText(sanPham.getDonGia()*10/100 +"");
+                binding.tvGiaGoc.setText(sanPham.getDonGia()+"");
+                binding.tvMota.setText(sanPham.getMoTa());
+                binding.tvTenSP.setText(sanPham.getTenSP());
+                Glide.with(getApplicationContext()).load(sanPham.getImg()).error(R.drawable.anhnen).into(binding.imgAnhHang);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
 
     }
 }
