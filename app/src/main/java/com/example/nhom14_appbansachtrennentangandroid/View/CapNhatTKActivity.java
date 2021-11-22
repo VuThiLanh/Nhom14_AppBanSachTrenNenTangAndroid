ab18bc4a230ea9fe3c9f0889ec4616c9824b9845
VuThiLanh <vuthilanh01634284279@gmail.com>
2021-11-22 09:35:46 +0700

    //

--- app/src/main/java/com/example/nhom14_appbansachtrennentangandroid/View/CapNhatTKActivity.java
+++ app/src/main/java/com/example/nhom14_appbansachtrennentangandroid/View/CapNhatTKActivity.java
@@ -1,16 +1,18 @@
 package com.example.nhom14_appbansachtrennentangandroid.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 
 import android.os.Bundle;
 
 import com.example.nhom14_appbansachtrennentangandroid.R;
 
 public class CapNhatTKActivity extends AppCompatActivity {
-
+    ViewDataBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cap_nhat_tkactivity);
+        binding= DataBindingUtil.setContentView(CapNhatTKActivity.this, R.layout.activity_cap_nhat_tkactivity);
     }
 }
