1066ed9c3dc35b3b64a640b772a0823f673adec8
VuThiLanh <vuthilanh01634284279@gmail.com>
2021-11-28 08:09:05 +0700

    //

---
+++ app/src/main/java/com/example/nhom14_appbansachtrennentangandroid/View/QuenmkActivity.java
@@ -0,0 +1,17 @@
+package com.example.nhom14_appbansachtrennentangandroid.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nhom14_appbansachtrennentangandroid.R;
+
+public class QuenmkActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quenmk);
+    }
+}
